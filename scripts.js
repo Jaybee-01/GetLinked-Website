18c9605037dd9cb1db5c4df17bdcbb45021ad5d2
Emmanuel Joshua Agbo <emmnanueljoshua348@gmail.com>
2025-04-09 16:53:30 +0100

    Creating a Replica Website

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const btnShow = document.getElementById("btnShow");
+const btnHide = document.getElementById("btnHide");
+
+btnShow.onclick = () => {
+  const items = document.querySelectorAll(".itemShow");
+  items.forEach((item) => {
+    item.style.display = "block";
+  });
+};
+
+btnHide.onclick = () => {
+  const items = document.querySelectorAll(".itemShow");
+  items.forEach((item) => {
+    item.style.display = "none";
+  });
+};
